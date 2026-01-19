49fd4cd2f3b0495ea7a31e0112b19328f9378814
Yehya El Hassan <yehya@aica.tech>
2026-01-19 15:22:16 +0100

    fix: setup config

--- pointnet2_ops/setup.py
+++ pointnet2_ops/setup.py
@@ -1,18 +1,43 @@
 import glob
+import os
 import os.path as osp
 
 from setuptools import find_packages, setup
-from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+from torch.utils.cpp_extension import (
+    BuildExtension,
+    CUDAExtension,
+    CUDA_HOME,
+)
 
 this_dir = osp.dirname(osp.abspath(__file__))
 _ext_src_root = osp.join("pointnet2_ops", "_ext-src")
-_ext_sources = glob.glob(osp.join(_ext_src_root, "src", "*.cpp")) + glob.glob(
-    osp.join(_ext_src_root, "src", "*.cu")
+
+_ext_sources = (
+    glob.glob(osp.join(_ext_src_root, "src", "*.cpp")) +
+    glob.glob(osp.join(_ext_src_root, "src", "*.cu"))
 )
 
 requirements = ["torch>=1.4"]
-
 exec(open(osp.join("pointnet2_ops", "_version.py")).read())
+
+ext_modules = []
+cmdclass = {}
+
+skip_cuda = os.environ.get("POINTNET2_SKIP_CUDA", "0") == "1"
+
+if (not skip_cuda) and CUDA_HOME is not None:
+    ext_modules.append(
+        CUDAExtension(
+            name="pointnet2_ops._ext",
+            sources=_ext_sources,
+            include_dirs=[osp.join(this_dir, _ext_src_root, "include")],
+            extra_compile_args={
+                "cxx": ["-O3"],
+                "nvcc": ["-O3", "-Xfatbin", "-compress-all"],
+            },
+        )
+    )
+    cmdclass["build_ext"] = BuildExtension
 
 setup(
     name="pointnet2_ops",
@@ -20,17 +45,7 @@ setup(
     author="Erik Wijmans",
     packages=find_packages(),
     install_requires=requirements,
-    ext_modules=[
-        CUDAExtension(
-            name="pointnet2_ops._ext",
-            sources=_ext_sources,
-            extra_compile_args={
-                "cxx": ["-O3"],
-                "nvcc": ["-O3", "-Xfatbin", "-compress-all"],
-            },
-            include_dirs=[osp.join(this_dir, _ext_src_root, "include")],
-        )
-    ],
-    cmdclass={"build_ext": BuildExtension},
+    ext_modules=ext_modules,
+    cmdclass=cmdclass,
     include_package_data=True,
 )
