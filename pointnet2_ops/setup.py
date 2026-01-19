1bf91f7fbeafabff81746a07d18d9bbfc968ce4f
Yehya El Hassan <yehya@aica.tech>
2026-01-19 17:34:38 +0100

    fix: library loading

--- pointnet2_ops/setup.py
+++ pointnet2_ops/setup.py
@@ -1,39 +1,14 @@
-import glob
-import os.path as osp
-
-from setuptools import find_packages, setup
-from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+from setuptools import setup, find_packages
 import os
 
-this_dir = osp.dirname(osp.abspath(__file__))
-_ext_src_root = osp.join("pointnet2_ops", "_ext-src")
-_ext_sources = glob.glob(osp.join(_ext_src_root, "src", "*.cpp")) + glob.glob(
-    osp.join(_ext_src_root, "src", "*.cu")
-)
-
-requirements = ["torch>=1.4"]
-os.environ["TORCH_CUDA_ARCH_LIST"] = "7.0 7.5 8.0 8.6 8.9"
-os.environ["CUDA_HOME"] = "/usr/local/cuda"
-
-exec(open(osp.join("pointnet2_ops", "_version.py")).read())
+# Read version from pointnet2_ops/_version.py
+exec(open(os.path.join("pointnet2_ops", "_version.py")).read())
 
 setup(
     name="pointnet2_ops",
     version=__version__,
     author="Erik Wijmans",
     packages=find_packages(),
-    install_requires=requirements,
-    ext_modules=[
-        CUDAExtension(
-            name="pointnet2_ops._ext",
-            sources=_ext_sources,
-            extra_compile_args={
-                "cxx": ["-O3"],
-                "nvcc": ["-O3", "-Xfatbin", "-compress-all"],
-            },
-            include_dirs=[osp.join(this_dir, _ext_src_root, "include")],
-        )
-    ],
-    cmdclass={"build_ext": BuildExtension},
+    install_requires=["torch>=1.4"],
     include_package_data=True,
 )
